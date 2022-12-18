ed8c05f8eda57eba4820670ee2871fc4046d6124
Ariel Luz <ariel.luz@ccc.ufcg.edu.br>
2022-12-18 19:56:21 -0300

    feat: add rust example

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+
+    let array:[i8;4] = [1,2,3,4];
+
+    println!("Position 1 have: {}",array[0]);
+    println!("Position 2 have: {}",array[1]);
+    println!("Position 3 have: {}",array[2]);
+    println!("Position 4 have: {}",array[3]);
+    
+    //Acessing a position that do not exist in our array 
+    println!("Position 5 have: {}",array[4]);
+
 }
